b461ff414e7f99482d9b5c7e04bd70e1b21ac534
Be Happy <mgy1608@mail.ru>
2016-07-22 17:38:25 +0300

    add body

---
+++ RG_AntiReuseCourier/body.js
@@ -0,0 +1,9 @@
+﻿﻿/*
+  License - Creative Commons Attribution-NoDerivatives 4.0 International License. (CC BY-ND 4.0)
+  Project - Real Grace
+  Author  - Starlight & Sleid
+  Script  - Anti Reuse Couerier
+  Version - 0.01
+  https://github.com/realgrace/Dota-2-Scripts
+*/
+ 
